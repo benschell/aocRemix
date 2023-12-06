09f92f412d93f4e48e0ba38fb289cd6480a55c74
Benjamin Schell <ben.schell@gmail.com>
2023-12-06 07:19:11 -0500

    2023 Day 06

--- app/routes/2023.06/route.tsx
+++ app/routes/2023.06/route.tsx
@@ -6,7 +6,7 @@ import { Form, Link, useActionData, useLoaderData, useNavigate } from "@remix-ru
 import { promises as fs } from "fs";
 import { getDirname } from "~/util";
 
-const dayNum = 0;
+const dayNum = 6;
 const dayStr = `${dayNum}`.padStart(2, '0');
 const slug = `2023.${dayStr}`;
 const name = `Day ${dayStr}`;
@@ -80,6 +80,51 @@ export async function action({
     // Read the json data file data.json
     rawInput = (await fs.readFile(`${__dirname}/../app/routes/${slug}/inputs/${inputFileName}`, "utf8")) as string;
   }
+
+  const lines = rawInput.split('\n');
+  const timeParts = lines[0].split(': ');
+  const distanceParts = lines[1].split(': ');
+  // Part 1:
+  // const times = timeParts[1].split(' ').filter((time) => time.length).map((time) => parseInt(time));
+  // const distances = distanceParts[1].split(' ').filter((distance) => distance.length).map((distance) => parseInt(distance));
+  // Part 2:
+  const times = [parseInt(timeParts[1].replaceAll(' ', ''))];
+  const distances = [parseInt(distanceParts[1].replaceAll(' ', ''))];
+  output.push(`Times ${JSON.stringify(times)} and Distances ${JSON.stringify(distances)}`);
+
+  let product = 1;
+  output.push(`Races: 1-${times.length}`);
+  for (let raceIdx = 0; raceIdx < times.length; raceIdx++) {
+    const timeAllowed = times[raceIdx];
+    const distanceToBeat = distances[raceIdx];
+    let leastTimeToWin = -1;
+    let mostTimeToWin = -1;
+    // Find the least time to win
+    for (let time = 0; time < timeAllowed-1; time++) {
+      const speed = time;
+      const distance = (timeAllowed - time) * speed;
+      // output.push(`For Race #${raceIdx+1}, if you hold the button for ${time}ms, the boat will travel ${distance}mm`);
+      if (distance > distanceToBeat) {
+        output.push(`For Race #${raceIdx}, found least time to win: ${time}`);
+        leastTimeToWin = time;
+        break;
+      }
+    }
+    // Find the most time to win
+    for (let time = timeAllowed; time > leastTimeToWin; time--) {
+      const speed = time;
+      const distance = (timeAllowed - time) * speed;
+      // output.push(`For Race #${raceIdx+1}, if you hold the button for ${time}ms, the boat will travel ${distance}mm`);
+      if (distance > distanceToBeat) {
+        output.push(`For Race #${raceIdx}, found most time to win: ${time}`);
+        mostTimeToWin = time;
+        break;
+      }
+    }
+    output.push(`For Race #${raceIdx+1}, there are ${mostTimeToWin - leastTimeToWin + 1} ways to beat the record`);
+    product *= (mostTimeToWin - leastTimeToWin + 1);
+  }
+  output.unshift(`Product: ${product}`);
 
   return json({ ok: true, rawInput, output: output.join('\n') });
 }
